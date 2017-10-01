ba18b4d0ea8783db5f09d2091b1e5efac27cef57
Monica  Karasek <mlkarasek@gmail.com>
2017-10-01 21:22:38 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,21 +1,20 @@
 
 
-def turn_count
-  counter = 0
-  board.each do |position|
-    if position == "X" || position == "O" || position == "X" || position == "O"
-      counter += 1
-    else
-end
-end
-counter
+def turn_count(board)
+  turns = 0
+  board.each do |space_on_board|
+    if space_on_board == "X" || space_on_board == "O" || space_on_board == "x" || space_on_board == "o"
+      turns += 1
+    end
+  end
+  turns
 end
 
 def current_player(board)
   turns = turn_count(board)
-   if turns % 2 == 0
-     "X"
-   else
-     "O"
+  if turns % 2 == 0
+    "X"
+  else
+    "O"
   end
 end
