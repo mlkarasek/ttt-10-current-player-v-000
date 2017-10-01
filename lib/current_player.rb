fda29f4ee92e6a58905ba4cdfa2ca32637204040
Monica  Karasek <mlkarasek@gmail.com>
2017-10-01 21:16:36 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,62 +1,15 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(user_input)
-  user_input.to_i-1
-end
-
-def move(board, index, current_player = "X")
-  board[index] = current_player
-end
-
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
-
-# Define your play method below
-def play(board)
-  counter = 0
-while counter < 9
-  puts "Last move!"
-  turn(board)
-  counter += 1
-end
-end
-
 def turn_count
   counter = 0
-  board.each do |token|
-    if token == "X" || token == "O"
+  board.each do |position|
+    if position == "X" || position == "O" || position == "X" || position == "O"
       counter += 1
+    else
 end
+end
+counter 
 end
 
 def current_player(board)
-  turn_num = turn_count(board) + 1
-  if turn_num.even? == true
-    return "O"
-  else
-    return "X"
+  !turn_count(board).even? ? "X" : "O"
   end
 end
