0e6aebc572954632835f661840815829cc733ba6
Monica  Karasek <mlkarasek@gmail.com>
2017-10-01 21:00:37 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,1 @@
-def turn_count(board)
-  @board.count {|token| token == "X"|| token == "O"}
-end
 
-def current_player(board)
-  if turn_count(board).even?
-    return "X"
-  else
-    return "O"
-  end
